2d3ff91e51e269b4089e01331c5c1ac903f789fd
yanovskyi <anton.yanovskyi@gmail.com>
2020-01-12 13:00:40 +0200

    init hw9

---
+++ FL12_HW9/homework/index.js
@@ -0,0 +1,64 @@
+const convert = (...args) => {
+  let arr = [];
+  for (let value of args) {
+    !(typeof value === 'string') || arr.push(+value);
+    !(typeof value === 'number') || arr.push(`${value}`);
+  }
+  return arr;
+};
+
+const executeforEach = (arr, func) => {
+  for (let item of arr) {
+    func(item);
+  }
+};
+
+const mapArray = (arr, func) => {
+  let result = [];
+  executeforEach(arr, item => result.push(func(+item)));
+  return result;
+};
+
+const filterArray = (arr, func) => {
+  let result = [];
+  executeforEach(arr, item => !func(item) || result.push(item));
+  return result;
+};
+
+const flipOver = str => {
+  let result = '';
+  for (let item of str) {
+    result = item + result;
+  }
+  return result;
+};
+
+const makeListFromRange = arr => {
+  let result = [];
+  for (let i = arr[0]; i <= arr[1]; i++) {
+    result.push(i);
+  }
+  return result;
+};
+
+const getArrayOfKeys = (obj, key) => {
+  let result = [];
+  executeforEach(obj, item => result.push(item[key]));
+  return result;
+};
+
+const substitute = arr => {
+  const num = 30;
+  return mapArray(arr, item => item < num ? '*' : item);
+};
+
+const getPastDay = (date, num) => {
+  const ms = 86400000;
+  let resultDate = new Date(date.getTime() - num * ms).getDate();
+  return resultDate;
+};
+
+const formatDate = date => {
+  return `${date.getFullYear()}/${date.getMonth() +
+    1}/${date.getDate()} ${date.getHours()}:${date.getMinutes()}`;
+};
