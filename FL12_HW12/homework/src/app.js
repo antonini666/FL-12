7161bdf9398d2d45bfe5bb314153d4bb9de6491f
yanovskyi <anton.yanovskyi@gmail.com>
2020-01-28 20:41:29 +0200

    commit

--- FL12_HW12/homework/src/app.js
+++ FL12_HW12/homework/src/app.js
@@ -1,4 +1,68 @@
+const mainPage = document.querySelector('.main-page');
+const addPage = document.querySelector('.add-page');
+const modifyPage = document.querySelector('.modify-page');
 
+const listTermContainer = document.querySelector('.main-page__items');
 
-// Your code goes here
+const addTermsBtn = document.querySelector('.add-btn');
 
+const addName = document.querySelector('.add-name');
+const addTerm = document.querySelector('.add-term');
+
+const saveTermBtn = document.querySelector('.add-page__save');
+const cancelTermBtn = document.querySelector('.add-page__cancel');
+
+const saveModifyBtn = document.querySelector('.modify-page__save');
+const cancelModifyBtn = document.querySelector('.modify-page__cancel');
+
+const termsArray = [];
+
+const hashList = {
+  mainHash: '#/main',
+  addHash: '#/add',
+  editHash: '#/modify/'
+};
+
+addTermsBtn.addEventListener('click', () => {
+  location.hash = hashList.addHash;
+  mainPage.classList.toggle('hide');
+  addPage.classList.toggle('hide');
+});
+
+cancelTermBtn.addEventListener('click', () => {
+  location.hash = hashList.mainHash;
+  mainPage.classList.toggle('hide');
+  addPage.classList.toggle('hide');
+});
+
+saveTermBtn.addEventListener('click', () => {
+  location.hash = hashList.mainHash;
+  mainPage.classList.toggle('hide');
+  addPage.classList.toggle('hide');
+  createNewTerm();
+  createItems()
+});
+
+function createNewTerm(el) {
+  let name = addName.value;
+  let term = addTerm.value;
+  if (name && term) {
+    const newTerm = {
+      name: name,
+      term: term
+    };
+    termsArray.push(newTerm);
+  }
+}
+
+function createItems() {
+  termsArray.forEach(el => {
+    const itemTitle = document.createElement('h3');
+    const itemContent = document.createElement('p');
+    itemTitle.innerText = `Name : ${el.name}`;
+    itemContent.innerText = `Term : ${el.term}`;
+
+    listTermContainer.appendChild(itemTitle);
+    listTermContainer.appendChild(itemContent);
+  });
+}
